f3d8b9733a7d1b466a8e56d36fd43d54e632a399
NathanMeulle <54913885+NathanMeulle@users.noreply.github.com>
2022-02-17 19:39:21 +0100

    Add router Vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,27 +1,40 @@
 <template>
-  <b-navbar toggleable="lg" class="navbar navbar-dark bg-primary justify-content-left">
+ <b-navbar :style="cssProps" id="nav" toggleable="lg" class="navbar navbar-dark justify-content-left">
       <Logo :imgSrc="logoUrl" class=""/>
-      <b-navbar-brand href="#"><Title :title="NavbarTitle"/></b-navbar-brand>
+      <b-navbar-brand href="#"><h2>{{NavbarTitle}}</h2></b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav class="d-lg-block">
-        <Menu :menuItems="MenuItems"/>
+        <b-navbar-nav>
+          <b-nav-item v-for="item in MenuItems" :key="item.message">
+            <router-link :to="item">{{item}}</router-link>
+          </b-nav-item>
+        </b-navbar-nav>
       </b-collapse>
-  </b-navbar>
+    </b-navbar>
 </template>
 
 <script>
 import Logo from './Logo.vue'
-import Title from './Title.vue'
-import Menu from './Menu.vue'
 
 export default {
   name: 'Navbar',
   components: {
     Logo,
-    Title,
-    Menu
   },
-  props: ['logoUrl', 'MenuItems', 'NavbarTitle']
+  props: ['logoUrl', 'MenuItems', 'NavbarTitle', "NavBarColor"],
+  computed: {
+    cssProps() {
+      return {
+        '--navbar-color': this.NavBarColor,
+      }
+    }
+  }
 }
 </script>
+<style scoped>
+  .navbar{
+    background-color: var(--navbar-color);
+  } 
+</style>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Home page</h1>
+    <Classement />
+  </div>
+</template>
+
+<script>
+import Classement from '../components/Classement.vue'
+export default {
+  name: 'Home',
+  components: {
+    Classement,
+  },
+}
+</script>
